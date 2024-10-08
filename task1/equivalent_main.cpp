cfb8c180c188dc0ba25ba9e4dd48c068e6748596
quuger <greb.05@bk.ru>
2024-10-08 19:41:44 +0300

    small fix

--- task1/equivalent_main.cpp
+++ task1/equivalent_main.cpp
@@ -13,25 +13,3 @@ int main(int argc, char* argv[]) {
     machine::solve_equivalent(ifs1, ifs2);
     return 0;
 }
-
-//int main(int argc, char* argv[]) {
-//    if (argc != 3) {
-//        std::cerr << "Invalid number of arguments\n";
-//    }
-//    machine::solve_equivalent(argv[1], argv[2]);
-//    return 0;
-//}
-//
-//void solve_equivalent(char *file1, char *file2) {
-//    Machine dfa1, dfa2;
-//
-//    std::ifstream ifs1(file1, std::ios::binary);
-//    dfa1.read(ifs1);
-//    ifs1.close();
-//
-//    std::ifstream ifs2(file2, std::ios::binary);
-//    dfa2.read(ifs2);
-//    ifs2.close();
-//
-//    std::cout << std::boolalpha << (dfa1 == dfa2);
-//}

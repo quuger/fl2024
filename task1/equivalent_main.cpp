4ebe3a876884502f97896edce8c8b09ce7230e01
quuger <greb.05@bk.ru>
2024-10-08 19:45:04 +0300

    bug fix

--- task1/equivalent_main.cpp
+++ task1/equivalent_main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Invalid number of arguments\n";
+        return 0;
     }
     std::ifstream ifs1(argv[1], std::ios::binary);
     std::ifstream ifs2(argv[2], std::ios::binary);
